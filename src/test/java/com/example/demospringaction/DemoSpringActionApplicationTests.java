4c0791a07da05854c49c8ca1a9832e1c0e453428
Razieh Akbari <rakbari@hypertvx.com>
2021-03-21 14:04:51 +0100

    Initial Commit

---
+++ src/test/java/com/example/demospringaction/DemoSpringActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospringaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
